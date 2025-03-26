a41aec4b0b199583852b248720d94a5a2a7df100
MominaRizwan <121957399+MominaRizwan@users.noreply.github.com>
2025-03-26 07:12:50 +0500

    Update server.js

--- server.js
+++ server.js
@@ -161,7 +161,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
